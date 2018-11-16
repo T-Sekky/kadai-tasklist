962d3fed514a3f195b212b7b396e5d3f3a5080b6
sekky <pocopocoboobee@gmail.com>
2018-11-16 07:57:44 +0000

    user login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,13 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all.page(params[:page])
+#    @tasks = Task.all.page(params[:page])
+    if logged_in?
+      @task = current_user.tasks.build  # form_for 用
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+    end
   end
 
   def show
@@ -13,15 +18,26 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'タスクが正常に投稿されました'
+      flash[:success] = 'タスクを投稿しました。'
       redirect_to @task
     else
-      flash.now[:danger] = 'タスクが投稿されませんでした'
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+      flash.now[:danger] = 'タスクの投稿に失敗しました。'
       render :new
     end
   end
+
+#   @task = Task.new(task_params)
+#    if @task.save
+#      flash[:success] = 'タスクが正常に投稿されました'
+#      redirect_to @task
+#    else
+#      flash.now[:danger] = 'タスクが投稿されませんでした'
+#      render :new
+#    end
+#  end
 
   def edit
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
-
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/create'
-
-#  root to: 'tasks#index'
-root to: 'toppages#index'
+  root to: 'tasks#index'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:new, :create]
   resources :tasks
 end
